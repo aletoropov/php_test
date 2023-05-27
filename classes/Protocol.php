d131224317304a34c04730eb31a23d57185f34a9
Alexandr Toropov <toropovsite@yandex.ru>
2023-05-28 02:14:33 +0300

    Работа через новый класс для базы данных

--- classes/Protocol.php
+++ classes/Protocol.php
@@ -5,9 +5,12 @@ use classes\Database;
 
 class Protocol
 {
+    /**
+     * @return array|false
+     */
     public function getAll()
     {
-        $db = Database::getInstance();
+        $db = new Database();
 
         $select = "SELECT * FROM protocol_table";
         return $db->fetchAll($select, __METHOD__);
@@ -17,31 +20,28 @@ class Protocol
      * @param int $number
      * @param string $date
      * @param string $person
-     * @param string $sing
-     * @return array|bool
+     * @param string $sign
+     * @return array|false|string
      */
-    public function addProtocol(int $number, string $date, string $person, string $sing)
+    public function addProtocol(int $number, string $date, string $person, string $sign)
     {
-        $db = Database::getInstance();
+        $db = new Database();
 
         $errors = array();
 
         if ($this->checkProtocolNumber($number)) {
-            $errors[] = 'Номер протокола уже есть';
+            $errors[] = $number . ' - такой номер протокола уже есть в базе';
         } else {
-            $insert = "INSERT INTO protocol_table (`number`, `date`, `person`, `sing`) 
-                       VALUES (:number, :person, :sing, :sing)";
-             if ($db->exec($insert, __METHOD__, [
-                ':number' => $number,
-                ':date'   => $date,
-                ':person' => $person,
-                ':sing'   => $sing,
-            ])) {
-                 return true;
-             } else {
-                 $errors[] = $db->errorInfo()[2];
-             }
+            $insert = "INSERT INTO protocol_table (number, date, person, sign) 
+                   VALUES (?, ?, ?, ?)";
 
+            $stmt = $db->pdo->prepare($insert);
+
+            if ($stmt->execute([$number, $date, $person, $sign])) {
+                 return $db->pdo->lastInsertId();
+             } else {
+                 $errors[] = $db->pdo->errorInfo()[2];
+             }
         }
 
         return $errors;
@@ -53,9 +53,9 @@ class Protocol
      */
     private function checkProtocolNumber($number)
     {
-        $db = Database::getInstance();
+        $db = new Database();
 
-        $select = "SELECT number FROM protocols WHERE `number` = :number";
+        $select = "SELECT number FROM protocol_table WHERE `number` = :number";
         return $db->fetchOne($select, __METHOD__, [':number' => $number]);
     }
 }
