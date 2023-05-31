2609ab30b3a426bd44043b3b768f991f7350b7da
Alexandr Toropov <toropovsite@yandex.ru>
2023-05-31 07:02:41 +0300

    Автозагрузка классов с помощью Composer

--- common.php
+++ common.php
@@ -2,6 +2,4 @@
 require "config.php";
 
 //TODO: сделать автозагрузку через Composer
-require "classes/Database.php";
-require "classes/Protocol.php";
-require "classes/Template.php";
+require "vendor/autoload.php";
